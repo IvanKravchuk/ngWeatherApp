0bff2540271cf764baed4d41f6c8f65157cfe9c7
Malyna Marian <malina.marian@gmail.com>
2018-11-04 22:27:02 +0200

    add newVar

--- src/app/components/prtest/prtest.component.ts
+++ src/app/components/prtest/prtest.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrtestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private newVar:number) { }
 
   ngOnInit() {
   }
