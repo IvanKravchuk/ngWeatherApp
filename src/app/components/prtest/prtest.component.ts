f2a5e55429d49e8297055e52846df5c856c6c1da
Malyna Marian <malina.marian@gmail.com>
2018-11-04 21:43:27 +0200

    Test Pull request

---
+++ src/app/components/prtest/prtest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prtest',
+  templateUrl: './prtest.component.html',
+  styleUrls: ['./prtest.component.css']
+})
+export class PrtestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
